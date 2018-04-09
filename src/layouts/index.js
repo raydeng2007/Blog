866e702f6b613477263ad0754bc338efa6680e17
Raymond <r6deng@uwaterloo.ca>
2018-04-09 19:46:56 -0400

    update content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,7 +10,7 @@ import Media from 'react-media'
 const TemplateWrapper = ({ children }) => (
     <div>
         <Helmet>
-            <title>Ray's BBall Blog</title>
+            <title>Full Court Analysis</title>
             <link href="https://fonts.googleapis.com/css?family=Jua" rel="stylesheet"/>
             <link rel="shortcut icon" href="./favicon.ico"/>
         </Helmet>
@@ -59,16 +59,23 @@ const TemplateWrapper = ({ children }) => (
                                 {children()}
                             </div>
                             <div  style={{ width: '210px'}}>
-                                <Sidebar
-                                    title="About the website"
-                                    description="Analyzing Current NBA teams and players using statistical visualization
-                                    tools like R and TABLEAU to represent trends. Hoping to anticipate performance patterns
+                                <SidebarTop
+                                    title="About the Blog"
+                                    description="This blog is going to be mainly about analyzing NBA teams and players using
+                                    statistical visualization  tools like R and TABLEAU to represent trends. Hoping to
+                                     anticipate performance patterns
                                     and using statistical modeling methods such as simple linear regression to anticipate and
                                     analyze current team standings and reasons behind it."
+
                                 />
-                                <Sidebar
-                                    title="About author"
-                                    description="I am a Data scientist and blah blah blah."
+                                <SidebarBot
+                                    title="About the Author"
+                                    description="Third year Honours Statistics student at the University of Waterloo
+                                    with a huge passion for data science and basketball. Feel free to shoot me an email
+                                     for suggestions about the blog. Also you can check out some of my other cool stuff
+                                     I built on my github/website"
+                                    link1 = 'https://mail.google.com/mail/?view=cm&fs=1&to=r6deng@edu.uwaterloo.ca'
+                                    link2 = 'http://www.raymonddeng.com'
                                 />
                             </div>
                         </div>
@@ -79,7 +86,7 @@ const TemplateWrapper = ({ children }) => (
     </div>
 );
 
-const Sidebar = (props) => (
+const SidebarBot = (props) => (
     <div
         className="side"
         style={{
@@ -92,7 +99,25 @@ const Sidebar = (props) => (
 
         }}
     >
-        <strong className="side-title">{props.title}</strong> <br/>{props.description}
+        <strong className="side-title">{props.title}</strong> <br/>{props.description}<br/>
+        <a href={props.link1}> Email</a> <a href={props.link2}>Website</a>
+    </div>
+);
+
+const SidebarTop = (props) => (
+    <div
+        className="side"
+        style={{
+            position:'relative',
+            border: '2px solid #e6e6e6',
+            maxWidth:960,
+            padding: '0.5rem',
+            marginBottom: '25px',
+            paddingRight:'0px'
+
+        }}
+    >
+        <strong className="side-title">{props.title}</strong> <br/>{props.description}<br/>
     </div>
 );
 
