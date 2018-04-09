5fe3c1b365ef5fe323f98ab6502108a10bdb4db2
Raymond <r6deng@uwaterloo.ca>
2018-04-08 21:42:12 -0400

    reconfigured for favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,24 @@ module.exports = {
                 name: 'pages',
             },
         },
-        `gatsby-transformer-remark`
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/favicon.png",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        }
     ],
 };
